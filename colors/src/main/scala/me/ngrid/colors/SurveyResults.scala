d7fa727a47bf382eeda18daa41ad1ca0230c0eba
Nick Gridinskiy <nikita.gridinskiy@gmail.com>
2018-05-16 01:56:34 -0700

    updating the results to be streamed

--- colors/src/main/scala/me/ngrid/colors/SurveyResults.scala
+++ colors/src/main/scala/me/ngrid/colors/SurveyResults.scala
@@ -1,8 +1,11 @@
 package me.ngrid.colors
 
+import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.alpakka.slick.scaladsl._
+import akka.stream.scaladsl.Source
+import me.ngrid.jiggly.colors.survey
 import me.ngrid.jiggly.colors.survey.Tables
 import slick.jdbc.SQLiteProfile.api._
 
@@ -11,14 +14,13 @@ import scala.concurrent.{Await, ExecutionContext}
 import scala.language.postfixOps
 
 object SurveyResults {
-//  lazy val db = Database.forConfig("db.survey-results")
+  //  lazy val db = Database.forConfig("db.survey-results")
 
-  def getResults(limit: Int)(implicit ec: ExecutionContext, s: SlickSession)  = {
+  def getResults(limit: Int)(implicit ec: ExecutionContext, s: SlickSession): Source[survey.Tables.UsersRow, NotUsed] = {
     val query = for (u <- Tables.Users) yield {
       u
     }
 
-//    db.stream(query.result)
     Slick.source(query.take(limit).result)
   }
 }
@@ -26,7 +28,7 @@ object SurveyResults {
 object PrintResults {
   implicit val system: ActorSystem = ActorSystem()
   implicit val mat: ActorMaterializer = ActorMaterializer()
-//  implicit val ec: ExecutionContextExecutor = system.dispatcher
+  //  implicit val ec: ExecutionContextExecutor = system.dispatcher
 
   implicit val session: SlickSession = SlickSession.forConfig("db.survey-results")
 
@@ -35,9 +37,9 @@ object PrintResults {
   def main(args: Array[String]): Unit = {
     import scala.concurrent.ExecutionContext.Implicits._
 
-//    val res = SurveyResults.getResults(5).runWith(Sink.ignore)
+    //    val res = SurveyResults.getResults(5).runWith(Sink.ignore)
     val res = SurveyResults.getResults(5).runForeach(println)
-    res.onComplete{ _ =>
+    res.onComplete { _ =>
       session.close()
       system.terminate()
     }
@@ -50,13 +52,13 @@ object PrintResults {
 
 object GenerateServeyResults {
   def main(args: Array[String]): Unit = {
-    slick.codegen.SourceCodeGenerator.main(
-      "slick.jdbc.SQLiteProfile" ::
-        "org.sqlite.JDBC" ::
-        "jdbc:sqlite:data/jigglydat.sqlite" ::
-        "colors/src/main/scala/" ::
-        "me.ngrid.jiggly.colors.survey" ::
-        Nil toArray
+    slick.codegen.SourceCodeGenerator.run(
+      "slick.jdbc.SQLiteProfile",
+      "org.sqlite.JDBC",
+      "jdbc:sqlite:data/jigglydat.sqlite",
+      "colors/src/main/scala/",
+      "me.ngrid.jiggly.colors.survey",
+      None, None, ignoreInvalidDefaults = true, None
     )
   }
 }
